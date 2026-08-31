5f6f74044a1c833a7bd23da50e86d52d2a0f0647
xf8410 <1421506586@qq.com>
2026-08-31 17:37:54 +0800

    v1.0.8（build 9）：体育场环境包

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.footballgame.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -21,7 +21,7 @@ android {
         }
     }
 
-    // 输出文件名带版本号：football-1.0.7-release.apk / football-1.0.7-debug.apk
+    // 输出文件名带版本号：football-1.0.8-release.apk / football-1.0.8-debug.apk
     applicationVariants.all {
         outputs.all {
             val variantOutput = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
